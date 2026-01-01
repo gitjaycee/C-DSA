2350eb5d11830735990f28be364fded7832920f4
James <gonzalesjamescarl5@gmail.com>
2026-01-01 17:56:16 +0800

    Task: Reversing a String

--- Stack/2.StringReverse.cpp
+++ Stack/2.StringReverse.cpp
@@ -1,1 +1,31 @@
 // Write a program that takes a list of numbers from the user and reverses the list using a stack.
+#include <iostream>
+#include <string>
+
+using namespace std;
+class  Reverse {
+    private:
+        int top;
+    public:
+        Reverse(string val) {
+            top = val.length() - 1;
+            for (int i = top; i >= 0; i--) {
+                cout << val[i];
+            }
+        }
+};
+
+int main()
+{
+    string userInput;
+    do
+    {
+        cout << "\nReverse String Program | Enter 0 to exit" << endl;
+        cout << "Enter a string to reverse: ";
+        cin >> userInput;
+        Reverse str(userInput);
+        
+    } while (userInput != "0");
+    
+    return 0;
+}
