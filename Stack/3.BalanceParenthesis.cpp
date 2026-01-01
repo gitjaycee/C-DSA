f3c975074cab69d884dc597af6d9f8686327a29e
James <gonzalesjamescarl5@gmail.com>
2026-01-01 21:50:15 +0800

    Task: Identying balanced parenthesis

--- Stack/3.BalanceParenthesis.cpp
+++ Stack/3.BalanceParenthesis.cpp
@@ -5,3 +5,73 @@
 // Output: Balanced
 // Input: ((a+b)
 // Output: Not Balanced
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class BalanceParenthesis {
+    private:
+        string stack;
+    public:
+        BalanceParenthesis(string val) {
+            for (int i = 0; i < val.length(); i++) {
+                if (isOpenBracket(val[i])) {
+                    stack.push_back(val[i]);
+                }
+                else if (isCloseBracket(val[i])) {
+                    if (isEmpty()) {
+                        cout << val << " is not Balance!\n";
+                        break;
+                    }
+                    else if (val[i] == ']' && stack[stack.length() - 1] == '[') {
+                        stack.pop_back();
+                    }
+                    else if (val[i] == '}' && stack[stack.length() - 1] == '{') {
+                        stack.pop_back();
+                    }
+                    else if (val[i] == ')' && stack[stack.length() - 1] == '(') {
+                        stack.pop_back();
+                    }
+                }
+            }
+            if (!isEmpty()) {
+                cout << val << " is not Balance!\n";
+            }
+            else {
+                cout << val << " is Balance!\n";
+            }
+            
+        }
+
+        bool isOpenBracket(char val) {
+           return (val == '(' || val == '[' || val == '{');
+        }
+        bool isCloseBracket(char val) {
+            return (val == ')' || val == ']' || val == '}');
+        }
+        bool isEmpty() {
+            if (stack.length() == 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+};
+
+int main()
+{
+    string str;
+    do
+    {
+
+        cout << "Balance parenthesis Program | Enter 0 to exit\n"
+            << "Enter a string: ";
+        cin >> str;
+        BalanceParenthesis balance(str);
+    } while (str != "0");
+    
+  return 0;
+}
